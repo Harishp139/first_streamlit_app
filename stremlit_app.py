a356841e6ed95dfd70021cb6ea7f625f594ff846
Harishp139 <131951138+Harishp139@users.noreply.github.com>
2023-04-27 16:21:30 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('My Parents new healthy Dinner')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
